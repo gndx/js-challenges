e333e1f791b4c92e6c39e802b963c0bd3ca56e21
Manuel Salvador <manu.sacr@hotmail.com>
2023-03-08 13:04:01 -0300

    3. Fibonacci solved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
-  // your code here
-}
+  let result = [0, 1];
+
+  for (let index = 2; index <= n; index++) {
+    result[index] = result[index - 2] + result[index - 1];
+  }
+
+  result.shift();
+  return result;
+};
 
 module.exports = fibonacci;
