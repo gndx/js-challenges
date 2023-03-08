d3fe3680f61cd352ddce6bd03a043227d120b7e1
Carlos Revetti <cdrn_19@hotmail.com>
2023-03-08 17:42:54 -0400

    :white_check_mark: add solutions

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (n) => {
-  // your code here
+  let succession = [1, 1];
+  // conditional from number 1
+  if(n == 1)
+    return [1];
+  // iteration from number 2
+  for( i = 2; i < n ; ++i){
+    succession[i] = succession[i - 1] + succession[i - 2];
+  }
+  // result 
+  return succession
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
-  // your code here
+  // check number
+  if(parseInt(number) < 2) return false;
+  if(parseInt(number) == 2) return true;
+  // if number is greater than 2, check primality
+  for(let i = 2 ; i < parseInt(number) ; ++i) {
+    if(parseInt(number) % i == 0) return false;
+  }
+  return true;
 }
 
 module.exports = trialDivision;
