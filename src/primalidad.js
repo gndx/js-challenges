62e82a6a13bf4dabd44851b0b01f4744286364b0
juan josé <jjmoralesz@correo.iue.edu.co>
2023-03-10 17:16:08 -0500

    Reto platzi

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
   // your code here
-}
+  number = Math.floor(number);
+  
+   if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+  
+};
 
 module.exports = trialDivision;
