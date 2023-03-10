ab3b5ca8ebd57295d610f05a61a65fb2ec4560e5
Rafael Livise <r.livise@hotmail.com>
2023-03-09 22:00:35 -0500

    feat: Factorial resuelto

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-  // your code here
+  //numbar = max
+  // numeber = 0 ; res = 1
+
+  let res = 1
+  for (let index = 1; index <= number; index++) {
+    res = res * index;
+  }
+  return res
+
 }
 
 module.exports = factorial;
