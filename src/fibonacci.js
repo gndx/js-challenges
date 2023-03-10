36de5dc59748841828645aa3ed37cf93e9c1c419
alberto sanchez <soy_uno_2008@htmail.com>
2023-03-10 14:53:00 -0600

    Reto Resuelto Alberto Sanchez

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
-  // your code here
+  let fibonacci = [0, 1];
+  while (fibonacci.length <= n) {
+    let lastItem = fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
+        fibonacci.push(lastItem);
+  }
+  fibonacci.shift()
+  return fibonacci;
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
-  // your code here
+  if (number < 2) {
+    return false;
+  }
+
+  if (number % 1 !== 0)
+  {
+    return false
+  }
+  
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = trialDivision;
