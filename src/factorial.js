07d686ce72509c720875e99a25467952b4a7d0cd
Edison Florez <edisonffh@gmail.com>
2023-03-10 16:21:10 +1300

    Reto JS ETH developer program. Nombre: Edison Florez, Usuario Platzi: edisonffh@gmail.com

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,16 @@
 const factorial = (number) => {
-  // your code here
-}
+  if (number < 0) {
+    return undefined;
+  }
+  let result = 1;
+  for (let i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+// console.log(factorial(5)); // 120
+// console.log(factorial(9)); // 362880
+// console.log(factorial(15)); // 1307674368000
 
 module.exports = factorial;
