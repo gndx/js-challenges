1bc4d80a98d137208bd5421148cb497bc6ca53e7
Ruben Monrroy <rubsoft@gmail.com>
2023-03-10 19:21:04 -0500

    Platzi Postulacion Ethereum Developer Program

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
-  // your code here
+    let fib = [1, 1]; 
+      if (n == 1) { fib =[1]; }
+      if (n <= 2) { return fib.slice(0, n); }
+      for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]; 
+      }
+      return fib;
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,24 @@
-const trialDivision = (number) => {
-  // your code here
+const trialDivision = (number) => {  
+  var es_numero_real;
+  es_numero_real = !(Number.isInteger(number));
+  if (es_numero_real == true) {
+    number=Math.floor(number);
+  }
+  if (number <= 1) {
+    return false;
+  }
+  if (number === 2 || number === 3) {
+    return true;
+  }
+  if (number % 2 === 0 || number % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i <= Math.sqrt(number); i += 6) {
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = trialDivision;
