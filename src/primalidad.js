eec4f6abccd9b8db7cd8bf273c6b9670155bcc09
Sebastian <sangetsu_9999@hotmail.com>
2023-03-10 21:48:23 -0500

    Retos terminados

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,26 @@
 const trialDivision = (number) => {
   // your code here
+  let a=0
+  for (i=1;i<=number;i++){
+
+    if(number%i == 0){
+      a=a+1;
+    }
+
+  }
+  if(number==1){
+    return false 
+  }
+  
+  else if (a==2){
+    return true
+  }
+
+  else {
+    return false
+  }
+
+
 }
 
 module.exports = trialDivision;
