0dc31a0c054244cd4817513e65598d9a4e202b30
daminnock <53588535+daminnock@users.noreply.github.com>
2023-03-11 19:22:01 +0100

    Add files via upload
    Challenge completed

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,29 @@
-const trialDivision = (number) => {
-  // your code here
-}
+// Next function calculates returns True if a number is prime. If it's not a prime then it returns false
 
-module.exports = trialDivision;
+function primalidad(number)
+{
+  if (number < 1)
+  {
+    return(false);
+  }
+  else if (number < 3)
+  {
+    return(true);
+  }
+  else
+  {
+  for(let i = 2; i <= number/3; i++)
+    {
+      if (number%i==0)
+      {
+        return(true);
+      }
+    }
+    return(false);
+  }
+};
+
+//console.log(primalidad(13)) //true
+//console.log(primalidad(14)) //false
+
+module.exports = primalidad;
