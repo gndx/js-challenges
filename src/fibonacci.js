173939cda1189eeeb9f6c4735e71db99e00f7b07
Mike071-4810 <reyeshernandezm071@gmail.com>
2023-03-12 03:36:47 -0400

    finalizado ejercicio fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = (n) => {
   // your code here
+
+    let a = 0, b = 1, temp; 
+        if (n == 0) 
+            return a; 
+       if (n == 1)  
+            return b; 
+    
+        while (n >= 0){ 
+            temp = a + b; 
+            a = b; 
+            b = temp;  
+            n--;       }     
+    
+       return b; 
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
-  // your code here
+  // your code here 
+      if (number < 2){ 
+        return false; 
+    }
+ 
+    let divisor = 2; 
+    while (divisor < number) { 
+        if (number % divisor == 0){ 
+         return false; // No es primo 
+     } else {  
+            divisor++; // Intenta con otro divisor 
+        }             
+   }
+
+   return true;   
 }
 
 module.exports = trialDivision;
