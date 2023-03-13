c20a30474018da9ce2abaee8889511316854effd
dmosc <osdavrm@gmail.com>
2023-03-13 16:24:08 -0600

    fix(tests): add code to fix non-passing tests.

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-  // your code here
-}
+  if (!Number.isInteger(number)) {
+    throw new Error("number must be a valid integer");
+  }
+
+  let aggMult = 1;
+
+  while (number) aggMult *= number--;
+
+  return aggMult;
+};
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = (n) => {
-  // your code here
-}
+  if (n <= 0 || !Number.isInteger(n)) {
+    throw new Error("n must be a valid integer greater than 0");
+  }
+
+  const seq = [];
+  let n1 = 0;
+  let n2 = 1;
+  let tmp;
+
+  while (n--) {
+    seq.push(n1 + n2);
+    tmp = n1;
+    n1 += n2;
+    n2 = tmp;
+  }
+
+  return seq;
+};
 
 module.exports = fibonacci;
