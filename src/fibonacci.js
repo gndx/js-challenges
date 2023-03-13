634d4e5205f3701015430c1277382108e2981bd7
Rafael Beltre <rbeltrecastro@gmail.com>
2023-03-12 21:51:53 -0400

    Update

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (n) => {
   // your code here
+  let a = 1;
+  let b =0;
+  let arreglo = [];
+
+  for (let i=1; i <= n; i++){
+    c = a + b;
+    a = b;
+    b =c;
+    arreglo.push(c);
+  } 
+
+  return arreglo;
 }
 
 module.exports = fibonacci;
