405eda5f8f9b0a79c8d51ecccb5eabdfffb6e515
NarcisaColcha <narcisacolcha@outlook.es>
2023-03-14 00:44:53 -0500

    [ADD] Retos ETH Dev Program resueltos: Fibonacci, Factorial y Primalidad - complete

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,28 @@
 const trialDivision = (number) => {
-  // your code here
+
+  let n = number;
+  let cont = 0;
+  
+  if (number < 2) {
+    return false;
+  } else {
+    for (let i = 1; i <= number; i++) {
+      if ((n % i) === 0) {
+        cont += 1;
+      }
+      //Break loop for
+      if (cont > 2) {
+        break;
+      }
+    }
+
+    if (cont === 2) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }  
 }
 
 module.exports = trialDivision;
