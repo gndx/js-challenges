da479b213650412b7df52b552e4514bd3bb1cbcc
neocarvajal <neocarvajal@gmail.com>
2023-03-16 13:32:10 -0400

    agregando solución

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,15 @@
-const factorial = (number) => {
-  // your code here
+const factorial = (numero) => {
+  if(numero < 0) {
+    return NaN;
+  }
+
+  let resultado = 1;
+
+  for (let i = 2; i <= numero; i++) {
+    resultado *= i;
+  }
+  
+  return resultado;
 }
 
 module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,26 @@
-const trialDivision = (number) => {
+const trialDivision = (numero) => {
   // your code here
+  if (numero < 2) {
+    return false;
+  }
+
+  const numeros = Array.from({ length: numero - 1 }, (_, index) => index + 2);
+
+  const limite = Math.sqrt(numero);
+  for (let i = 2; i <= limite; i++) {
+    if (numeros.includes(i)) {
+      for (let j = i * i; j <= numero; j += i) {
+        const index = numeros.indexOf(j);
+        if (index !== -1) {
+          numeros.splice(index, 1);
+        }
+      }
+    }
+  }
+
+  return numeros.includes(numero);
 }
+
+
 
 module.exports = trialDivision;
