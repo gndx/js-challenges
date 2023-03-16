233b5ad872d2ca63a112778c17bcbb8b80a87eff
salem-pm <saulr@protonmail.com>
2023-03-16 00:23:30 -0600

    Problemas resueltos
    [Add] Se crearon 3 funciones para el reto del programa

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-  // your code here
+  let num = parseInt(number);
+  if (isNaN(num)) {
+    return;
+  }else {
+    if (num === 0 || num === 1) return 1; 
+    else return num * factorial(num - 1);
+  }
 }
 
 module.exports = factorial;
