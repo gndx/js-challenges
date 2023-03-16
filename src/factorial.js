118924efb60586ca4f5c0ff21b1e70ae2b81cb2e
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-03-16 12:13:03 -0500

    Challenge completed

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,13 @@
-const factorial = (number) => {
+const factorial = ( number ) => {
   // your code here
+  if ( number === 0 || number === 1)  {
+    return 1
+  }
+  let facto = 1
+  for ( let index = 2; index <= number; index++ ) {
+    facto = facto * index
+  }
+  return facto
 }
 
 module.exports = factorial;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,14 @@
-const trialDivision = (number) => {
+const trialDivision = ( numero ) => {
   // your code here
+  if ( numero <= 1 || numero % 1 !== 0 ) {
+    return false
+  }
+  for ( let i = 2; i <= Math.sqrt( numero ); i++ ) {
+    if ( numero % i === 0 ) {
+      return false
+    }
+  }
+  return true
 }
 
 module.exports = trialDivision;
