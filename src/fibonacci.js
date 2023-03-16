2a5b472d59246ce9b0279744a8856797bfbdc914
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-03-16 10:01:40 -0500

    feat: adding js challenges solutions

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
-  // your code here
+  if (n === 1) {
+    return [1]
+  }
+  if (n === 2) {
+    return [1, 1]
+  }
+  const serie = [1, 1]
+  for (let i = 2; i < n; i++) {
+    serie.push(serie[i - 1] + serie[i - 2])
+  }
+  return serie
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci
