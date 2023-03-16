d9afa5c739d90cebb8e5ac101cf7c198ba85af67
Marco-FonsecaA <128007923+Marco-FonsecaA@users.noreply.github.com>
2023-03-15 23:46:04 -0500

    Update fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (n) => {
-  // your code here
+    var fibo = [1, 1];
+
+    if (!(number < 0)) {
+
+        for (var i = 2; i <= number; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+
+        fibo.pop();
+        return fibo;
+    }
 }
 
 module.exports = fibonacci;
