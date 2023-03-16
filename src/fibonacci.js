62f16c49e44f9e637798e732404fc5976c8446d4
Adripaluja <adriana.patricia84@gmail.com>
2023-03-16 16:03:34 -0500

    @adripaluja - Reto plazi ethereum

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = (n) => {
-  // your code here
+  var a = 1, b = 0, temp;
+  var array=new Array();
+
+  while (n-1 >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    array.push(b);
+    n--;
+  }
+
+  return array;
 }
 
 module.exports = fibonacci;
