a74bbe6b2be584548095b8285881065f4aa1ffa8
karina zwenger <karina.zwenger@live.com.ar>
2023-03-16 12:50:38 -0300

    js-challenges

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,24 @@
 const fibonacci = (n) => {
-  // your code here
-}
+	//primero determino que n sea un numero entero positivo
+	if (typeof n !== "number")
+		return console.error(`${n} is not of type number❗`);
+	if (n <= 0 || !Number.isInteger(n) || n === " " || n === "")
+		return console.error(`${n} is not a positive integer❗`);
+
+	//creo un array vacio para ir agregando los numeros de la secuencia
+	let fibo = [];
+
+	//creo un bucle que recorra el array hasta que su longitud sea igual a n
+	for (let i = 0; i < n; i++) {
+		//si la longitud del array es menor a 2, agrego el numero 1
+		if (fibo.length < 2) {
+			fibo.push(1);
+		} else {
+			//si la longitud del array es mayor o igual a 2, agrego la suma de los dos ultimos numeros del array
+			fibo.push(fibo[i - 2] + fibo[i - 1]);
+		}
+	}
+	return fibo;
+};
 
 module.exports = fibonacci;
