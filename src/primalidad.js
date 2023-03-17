4c789efd5211eca48d87f73c22a33890da81e70d
ED REDR <101226848+Xpedr2@users.noreply.github.com>
2023-03-16 23:41:38 -0300

    Uploaded scripts

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,20 @@
 const trialDivision = (number) => {
-  // your code here
+var flotante = String(number);
+if(flotante.includes(".")) {
+  return false;
+} else {
+    if (number < 2) {
+      return false;
+    }
+
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
+  console.log(trialDivision(4));
 
 module.exports = trialDivision;
