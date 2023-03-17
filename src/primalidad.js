dd70c9358b2e686cbcdf070dafde404933ce0fb2
Denisse Garcia <denissegarcia@Denisses-MacBook-Air.local>
2023-03-16 18:23:49 -0600

    :egg: Reto completado

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,27 @@
 const trialDivision = (number) => {
-  // your code here
+  // Excepcion para <=1 y float
+  if((number <= 1) || (number%1 !== 0)){
+    return false;
+  }
+
+  // Probar para (n/2)+1 no tiene sentido.
+  if(number == 2){
+    return true;
+  }
+
+  var esPrimo = true;
+  var n = Math.floor(number/2)
+
+  while(n>1){
+    if(number%n == 0){
+      esPrimo = false;
+      break;
+    }
+    n--;
+  }
+
+  return esPrimo
+  // \_0>
 }
 
 module.exports = trialDivision;
