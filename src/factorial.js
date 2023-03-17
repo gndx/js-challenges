f565336c05aed1c037ad2282b0acc90a7797d293
Maricela Chavarria <chavita.angelical@gmail.com>
2023-03-17 00:04:27 -0400

    challenge completed

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,8 @@
-const factorial = (number) => {
-  // your code here
+const factorial = (n) => {
+  if (n==0 | n==1)
+    return 1
+  else
+    return n * (factorial(n-1))
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = (n) => {
-  // your code here
+const secuenciaFibo = (n) => {
+  if (n <= 1)
+    return n
+  else
+    return parseInt(secuenciaFibo(n-1))+parseInt(secuenciaFibo(n-2))
+}
+
+const fibonacci = (n) =>{
+  let fibo = []
+  for(let i=1; i<=n; i++){
+    fibo.push(secuenciaFibo(i))
+  }
+  return fibo
 }
 
 module.exports = fibonacci;
