df73305bad3a6063e83b87e9a3593938a2c908cc
vladimircod <64986308+vladimircod@users.noreply.github.com>
2023-03-16 22:21:38 -0500

    Update factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-  // your code here
+  var resultado = number;
+  if (number === 0 || number === 1) 
+    return 1; 
+  while (number > 1) { 
+    number--;
+    result *= number;
+  }
+  return resultado;
 }
-
 module.exports = factorial;
