6f96a80b00b837e22becc9bb9907ade534ef5184
Rudy Ramirez <ruramirez@nuevatel.com>
2023-03-16 20:19:01 -0400

    reto completado

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,17 @@
+const fib = (n) => {
+  if(n<=1) {
+    return n
+  }else{
+    return parseInt(fib(n-1)) + parseInt(fib(n-2))
+  }
+}
+
 const fibonacci = (n) => {
-  // your code here
+  let res = []
+  for(let i = 1; i<=n; i++){
+    res.push(fib(i))
+  }
+  return res
 }
 
 module.exports = fibonacci;
