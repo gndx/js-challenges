bd810b1cf0ea54bf5eec6e9b2117c1e6dcafadfa
Miguel <miguelclavijo03@gmail.com>
2023-03-16 22:57:57 -0500

    my challenge

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
-  // your code here
+  if (n==1) {
+    return [1]
+  }
+  const fibArray = [1, 1];
+  for (let i = 2; i < n; i++) {
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+  }
+  return fibArray;
 }
 
 module.exports = fibonacci;
