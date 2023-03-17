4694c4131e3e5479d4241e08a0674227416411d5
vladimircod <64986308+vladimircod@users.noreply.github.com>
2023-03-16 22:22:44 -0500

    Update primalidad.js

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,23 @@
-const trialDivision = (number) => {
-  // your code here
+const trialDivision = (number) => {  
+  var esReal;
+  esReal = !(Number.isInteger(number));
+  if (esReal == true) {
+    number=Math.floor(number);
+  }
+  if (number <= 1) {
+    return false;
+  }
+  if (number === 2 || number === 3) {
+    return true;
+  }
+  if (number % 2 === 0 || number % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i <= Math.sqrt(number); i += 6) {
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
-
 module.exports = trialDivision;
