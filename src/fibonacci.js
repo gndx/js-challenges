cb1b528813a62a5d8865ec8f8924e1db6aaf501b
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-03-17 22:15:31 -0400

    done

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (n) => {
   // your code here
+  if (n === 0) return [];
+  const fibArr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibArr[i] = fibArr[i-1] + fibArr[i-2];
+  }
+  return fibArr.slice(1, n+1);
 }
-
 module.exports = fibonacci;
+
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
   // your code here
+  if (number < 2 || number % 1 !== 0) {
+    return false;
+  }
+  const primes = new Array(number + 1).fill(true);
+  primes[0] = false;
+  primes[1] = false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (primes[i]) {
+      for (let j = i * i; j <= number; j += i) {
+        primes[j] = false;
+      }
+    }
+  }
+  return primes[number];
 }
 
 module.exports = trialDivision;
