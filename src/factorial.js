15c1f4dad5b4865ec90a8478beb26e901a237d3c
Davidalmaz <davidalmandoz@gmail.com>
2023-03-17 22:49:02 -0400

    Challenge Done

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,19 @@
 const factorial = (number) => {
   // your code here
+  let result = number;
+  if (number < 0) { // Con nuestra primera condición descartamos todos los valores negativos.
+    return -1;
+  }
+  else if (number == 0) { // Con nuestra segunda condición mandamos la respuesta para el primer caso factorial(0).
+    return 1;
+  }
+  else {  // Con nuestra última condición realizamos la multiplicación del valor original con los valores inferiores, reduciendo la variable original hasta llegar a 1 que es cuando obtendriamos la respuesta correcta.
+    while (number != 1) {
+      number--;
+      result *= number;
+    }
+    return result;
+  }
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,17 @@
 const fibonacci = (n) => {
   // your code here
+  if (n < 1) { // Con nuestra primera condición descartamos todos los valores negativos y 0.
+    return -1;
+  }
+  else {  // Con nuetra última condición declaramos un arreglo con los valores iniciales y un arreglo con nuestro primer escenario fibonacci(1), para luego hacer un ciclo que recorre desde 2 hasta el valor del número ingresado, realizando la suma del valor fibonacci actual y el anterior, para guardarlo en la posición actual del arreglo y hacer un push para guardar el resultado en el arreglo final que será retornado al terminar el ciclo.
+    let arr = [0, 1];
+    let result = [1];
+    for(let i = 2; i <= n; i++) {
+      arr[i] = arr[i-2] + arr[i -1]
+      result.push(arr[i]);
+    }
+    return result;
+  }
 }
 
 module.exports = fibonacci;
