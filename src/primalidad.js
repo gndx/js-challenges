a4048cfe6c14d56329356a2decec0713759b5f33
jubuenor <jubuenor@unal.edu.co>
2023-03-22 12:04:05 -0500

    solutions

--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,10 @@
 const trialDivision = (number) => {
   // your code here
+  if(Math.floor(number)-number)return false
+  for(let i=2;i<=Math.sqrt(number);i++){
+      if(number%i===0) return false;
+  }
+  return number>1;
 }
 
 module.exports = trialDivision;
