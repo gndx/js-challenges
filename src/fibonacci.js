55fa96972d277627ca0d2ef88d085cf7fdfa0563
Juan Carlos <50887177+jucnunezgom@users.noreply.github.com>
2023-03-24 22:18:52 -0500

    Update fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,8 @@
 const fibonacci = (n) => {
-  // your code here
+  const arr = [0, 1];
+  for (let i = 2; i < n; i++)
+    arr.push(arr[i-1] + arr[i-2]);
+  return n > 0 ? arr.slice(0, n) : [];
 }
 
 module.exports = fibonacci;
