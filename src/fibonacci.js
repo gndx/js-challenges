cd5bd7da45dd30d4983919b60c6f3063b57b8016
Felipe <dfzunigah@unal.edu.co>
2023-03-24 23:32:54 -0500

    Update fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (n) => {
-  // your code here
+  // Forma recursiva de Fibonacci
+  let secuencia = [1, 1];
+  if(n == 1)
+    return [1];
+  for( i = 2; i < n ; ++i){
+    secuencia[i] = secuencia[i - 1] + secuencia[i - 2];
+  }
+  return secuencia
 }
 
 module.exports = fibonacci;
